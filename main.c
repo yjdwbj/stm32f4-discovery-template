a3988d7c2a815ad5fe0e080b3535213f58e64c7b
yjdwbj <yjdwbj@gmail.com>
2016-06-06 00:08:53 +0800

    fixed usart1

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /**
   ******************************************************************************
-  * @file    IO_Toggle/main.c 
+  * @file    IO_Toggle/main.c
   * @author  MCD Application Team
   * @version V1.0.0
   * @date    19-September-2011
@@ -16,11 +16,13 @@
   * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
   *
   * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************  
-  */ 
+  ******************************************************************************
+  */
 
 /* Includes ------------------------------------------------------------------*/
+#include "stdio.h"
 #include "stm32f4_discovery.h"
+
 
 /** @addtogroup STM32F4_Discovery_Peripheral_Examples
   * @{
@@ -28,7 +30,7 @@
 
 /** @addtogroup IO_Toggle
   * @{
-  */ 
+  */
 
 /* Private typedef -----------------------------------------------------------*/
 GPIO_InitTypeDef  GPIO_InitStructure;
@@ -45,57 +47,169 @@ void Delay(__IO uint32_t nCount);
   * @param  None
   * @retval None
   */
+
+
+#define MAX_STRLEN 128 // this is the maximum string length of our string in characters
+volatile char received_string[MAX_STRLEN+1]; // this will hold the recieved string
+
+
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+void USART1_Init(void)
+{
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+    //RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,ENABLE);
+
+
+
+    //GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 | GPIO_Pin_7 ;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    //GPIO_Init(GPIOA,&GPIO_InitStructure);
+    GPIO_Init(GPIOB,&GPIO_InitStructure);
+
+    GPIO_PinAFConfig(GPIOB,GPIO_PinSource6,GPIO_AF_USART1);
+    GPIO_PinAFConfig(GPIOB,GPIO_PinSource7,GPIO_AF_USART1);
+
+    //GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1);
+    //GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1);
+
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_Init(USART1,&USART_InitStructure);
+
+
+
+    USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
+
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    USART_Cmd(USART1,ENABLE);
+
+
+}
+
+
+
+
+void USART_puts(USART_TypeDef* USARTx, volatile char *s){
+
+	while(*s){
+		// wait until data register is empty
+		while( !(USARTx->SR & 0x00000040) );
+		USART_SendData(USARTx, *s);
+		*s++;
+	}
+}
+
+void USART1_IRQHandler(void){
+
+	// check if the USART1 receive interrupt flag was set
+	if( USART_GetITStatus(USART1, USART_IT_RXNE) ){
+
+		static uint8_t cnt = 0; // this counter is used to determine the string length
+		char t = USART1->DR; // the character from the USART1 data register is saved in t
+
+		/* check if the received character is not the LF character (used to determine end of string)
+		 * or the if the maximum string length has been been reached
+		 */
+		if( (t != '\n') && (cnt < MAX_STRLEN) ){
+			received_string[cnt] = t;
+			cnt++;
+		}
+		else{ // otherwise reset the character counter and print the received string
+			cnt = 0;
+			USART_puts(USART1, received_string);
+		}
+	}
+}
+
+
+
 int main(void)
 {
-  /*!< At this stage the microcontroller clock setting is already configured, 
-       this is done through SystemInit() function which is called from startup
-       file (startup_stm32f4xx.s) before to branch to application main.
-       To reconfigure the default setting of SystemInit() function, refer to
-        system_stm32f4xx.c file
-     */
+    /*!< At this stage the microcontroller clock setting is already configured,
+         this is done through SystemInit() function which is called from startup
+         file (startup_stm32f4xx.s) before to branch to application main.
+         To reconfigure the default setting of SystemInit() function, refer to
+          system_stm32f4xx.c file
+       */
 
-  /* GPIOD Periph clock enable */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    /* GPIOD Periph clock enable */
 
-  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-  while (1)
-  {
-    /* PD12 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_12);
-    
-    /* Insert delay */
-    Delay(0x3FFFFF);
-    
-    /* PD13 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_13);
-    
-    /* Insert delay */
-    Delay(0x3FFFFF);
-  
-    /* PD14 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_14);
-    
-    /* Insert delay */
-    Delay(0x3FFFFF);
-    
-    /* PD15 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_15);
-    
-    /* Insert delay */
-    Delay(0x7FFFFF);
-    
-    GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
-    
-    /* Insert delay */
-    Delay(0xFFFFFF);
-  }
+   // NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    USART1_Init();
+    USART_puts(USART1, "Init complete! Hello World!\r\n");
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+    /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+    while (1)
+    {
+        /* PD12 to be toggled */
+        GPIO_SetBits(GPIOD, GPIO_Pin_12);
+
+        /* Insert delay */
+        Delay(0x3FFFFF);
+
+        /* PD13 to be toggled */
+       GPIO_SetBits(GPIOD, GPIO_Pin_13);
+
+        /* Insert delay */
+        Delay(0x3FFFFF);
+
+        /* PD14 to be toggled */
+        GPIO_SetBits(GPIOD, GPIO_Pin_14);
+
+        /* Insert delay */
+        Delay(0x3FFFFF);
+
+        /* PD15 to be toggled */
+        GPIO_SetBits(GPIOD, GPIO_Pin_15);
+
+        /* Insert delay */
+        Delay(0x7FFFFF);
+
+        GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+
+        /* Insert delay */
+        Delay(0xFFFFFF);
+
+      //  Delay(500);
+      //  USART_puts(USART1, "Init complete! Hello World!\r\n");
+
+
+    }
 }
 
 /**
@@ -105,9 +219,9 @@ int main(void)
   */
 void Delay(__IO uint32_t nCount)
 {
-  while(nCount--)
-  {
-  }
+    while(nCount--)
+    {
+    }
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -120,23 +234,23 @@ void Delay(__IO uint32_t nCount)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+{
+    /* User can add his own implementation to report the file name and line number,
+       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-  /* Infinite loop */
-  while (1)
-  {
-  }
+    /* Infinite loop */
+    while (1)
+    {
+    }
 }
 #endif
 
 /**
   * @}
-  */ 
+  */
 
 /**
   * @}
-  */ 
+  */
 
 /******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
